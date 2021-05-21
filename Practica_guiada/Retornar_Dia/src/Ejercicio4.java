9395946b68459d3719b1c24194ad514353bcd325
enzo <enzostefani507@gmail.com>
2021-05-20 22:59:58 -0300

    Resolucion Ejer4: Dia de la semana

---
+++ Practica_guiada/Retornar_Dia/src/Ejercicio4.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Ejercicio4 {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner (System.in);
+        int dia = scan.nextInt();
+        scan.close();
+
+        switch(dia){
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Lunes");
+                break;
+            case 3:
+                System.out.println("Martes");
+                break;
+            case 4:
+                System.out.println("Miercoles");
+                break;
+            case 5:
+                System.out.println("Jueves");
+                break;
+            case 6:
+                System.out.println("Viernes");
+                break;
+            case 7:
+                System.out.println("Sabado");
+                break;
+        }
+
+    }
+}
