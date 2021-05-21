9b59a9f78e66b00ac78b16742e79bc206264ff8a
enzo <enzostefani507@gmail.com>
2021-05-21 12:55:28 -0300

    Error scan close solucionado, permite iterar sobre los ejercicios

--- Practica_guiada/Combinando_cosas_v2/src/App.java
+++ Practica_guiada/Combinando_cosas_v2/src/App.java
@@ -5,11 +5,11 @@ public class App {
     public static String menu = "Seleccione una opcion: 2 ,3, 4, 5";
     public static String separador = "=========================================================";
     public static void main(String[] args) throws Exception {
-        System.out.println(separador);
-        System.out.println(menu);
-        Scanner scan = new Scanner(System.in);
         int continuar = 1;
+        Scanner scan = new Scanner(System.in);
         do{
+            System.out.println(separador);
+            System.out.println(menu);
             int opcion = scan.nextInt();
             switch (opcion){
                 case 2:
@@ -45,7 +45,6 @@ public class App {
         System.out.println("El primer nro ingresado es: " + b);
         System.out.println("El primer nro ingresado es: " + c);
  
-         scan.close();
     }
 
     public static void Ejercicio3() throws Exception {
@@ -53,7 +52,7 @@ public class App {
         System.out.println("Ejercicio3: Ingrese una calificacion y la interpreto");
         Scanner scan = new Scanner (System.in);
         int nota = scan.nextInt();
-        scan.close();
+
         if (nota > 92){
             System.out.println("Excelente");
         }else if (nota > 84){
@@ -72,7 +71,6 @@ public class App {
         System.out.println("Ejercicio5: Ingrese un numero y lo relaciono con un día");
         Scanner scan = new Scanner (System.in);
         int dia = scan.nextInt();
-        scan.close();
 
         switch(dia){
             case 1:
@@ -113,6 +111,5 @@ public class App {
             System.out.println("Si desea presione ingresar más coloque 1, sino otro valor");
             continuar = scan.nextInt();
         } while (continuar == 1);
-        scan.close();
     }
 }
