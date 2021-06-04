792b466e509af159599bbcf3b4cfb106ca39fdf8
enzo <enzostefani507@gmail.com>
2021-06-04 14:36:28 -0300

    Correccion ej5

--- Clase030621/src/App.java
+++ Clase030621/src/App.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
